03aed1da703dd11969a24a3e14853a7bb0967161
Dmitry Bogomolov <Dmitry.Bogomolov.mail@gmail.com>
2019-03-03 18:54:27 +0300

    Refactor (#1)
    * Add initial tests
    
    * Add more tests
    
    * Update some names
    
    * Update lint settings

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ rl.emitKeypressEvents(cin);
 function renderList(items, printItem, focus, tags) {
     let lineCount = 0;
     items.forEach((item, i) => {
-        const lines = printItem(item, i, tags.has(i)).split('\n');
+        const lines = String(printItem(item, i, tags.has(i))).split('\n');
         lineCount += lines.length;
         const focusCh = i === focus ? '-' : ' ';
         const tagCh = tags.has(i) ? '*' : ' ';
@@ -39,16 +39,16 @@ const defaultHandlers = {
     'down': ({ index, setIndex }) => {
         setIndex(index + 1);
     },
-    'space': ({ index, setTag }) => {
-        setTag(index);
+    'space': ({ index, toggleCheck }) => {
+        toggleCheck(index);
     },
-    'return': ({ close }) => {
-        close();
+    'return': ({ end }) => {
+        end();
     },
 };
 
-function mutlipleTags(tags) {
-    const items = new Set(tags);
+function mutlipleChecks(checks) {
+    const items = new Set(checks);
     return {
         set(value) {
             if (items.has(value)) {
@@ -66,8 +66,8 @@ function mutlipleTags(tags) {
     };
 }
 
-function singleTag(tags) {
-    let item = tags >= 0 ? Number(tags) : NaN;
+function singleCheck(check) {
+    let item = check >= 0 ? Number(check) : NaN;
     return {
         set(value) {
             if (item === value) {
@@ -85,41 +85,46 @@ function singleTag(tags) {
     };
 }
 
+function verifyIndex(value, count) {
+    return 0 <= value && value < count;
+}
+
 function printList(items, options = {}) {
     const printItem = options.printItem || String;
+    const itemCount = items.length;
     let lineCount = 0;
-    let itemIndex = 0 <= options.index && options.index < items.length ? Number(options.index) : 0;
-    const tags = (options.singleTag ? singleTag : mutlipleTags)(options.tags);
-    const refresh = () => {
+    let index = verifyIndex(options.index, itemCount) ? Number(options.index) : 0;
+    const checks = (options.singleCheck ? singleCheck : mutlipleChecks)(options.checks);
+    function refresh() {
         clearList(lineCount);
-        lineCount = renderList(items, printItem, itemIndex, tags);
-    };
-    const setIndex = (arg) => {
+        lineCount = renderList(items, printItem, index, checks);
+    }
+    function setIndex(arg) {
         const newIndex = Number(arg);
-        if (newIndex !== itemIndex && 0 <= newIndex && newIndex < items.length) {
-            itemIndex = newIndex;
+        if (newIndex !== index && verifyIndex(newIndex, itemCount)) {
+            index = newIndex;
             refresh();
         }
-    };
-    const setTag = (arg) => {
+    }
+    function toggleCheck(arg) {
         const newIndex = Number(arg);
-        if (0 <= newIndex && newIndex < items.length) {
-            tags.set(newIndex);
+        if (verifyIndex(newIndex, itemCount)) {
+            checks.set(newIndex);
             refresh();
         }
-    };
+    }
     return new Promise((resolve, reject) => {
-        const dispose = () => {
+        function dispose() {
             cin.off('keypress', handle);
             cin.setRawMode(false);
             clearList(lineCount);
-        };
-        const close = (status) => {
+        }
+        function end(note) {
             dispose();
-            resolve({ status, index: itemIndex, tags: tags.data() });
-        };
+            resolve({ note, index, checks: checks.data() });
+        }
         const handlers = Object.assign({}, defaultHandlers, options.handlers);
-        const handle = (key, data) => {
+        function handle(key, data) {
             if (data.sequence === ESC || data.sequence === CTRLC) {
                 dispose();
                 reject(new Error('Canceled'));
@@ -127,12 +132,12 @@ function printList(items, options = {}) {
             }
             const handler = handlers[data.name];
             if (handler) {
-                handler({ index: itemIndex, setIndex, setTag, close });
+                handler({ index, setIndex, toggleCheck, end });
             }
-        };
+        }
         cin.setRawMode(true);
         cin.on('keypress', handle);
-        lineCount = renderList(items, printItem, itemIndex, tags);
+        lineCount = renderList(items, printItem, index, checks);
     });
 }
 
--- index.test.js
+++ index.test.js
@@ -1,7 +1,464 @@
+const mockOn = jest.fn();
+const mockOff = jest.fn();
+const mockSetRawMode = jest.fn();
+const mockWrite = jest.fn();
+const mockMoveCursor = jest.fn();
+const mockClearLine = jest.fn();
+
+Object.assign(process.stdin, {
+    on: mockOn,
+    off: mockOff,
+    setRawMode: mockSetRawMode,
+});
+Object.assign(process.stdout, {
+    write: mockWrite,
+});
+
+jest.mock('readline', () => ({
+    emitKeypressEvents: () => {},
+    moveCursor: mockMoveCursor,
+    clearLine: mockClearLine,
+}));
+
 const list = require('./index');
 
 describe('list', () => {
-    it('nope', () => {
+    afterEach(jest.resetAllMocks);
 
+    function emitKeypress(data) {
+        mockOn.mock.calls[0][1](null, data);
+    }
+
+    function emitReturn() {
+        emitKeypress({ name: 'return' });
+    }
+
+    function expectPrintedLines(...lines) {
+        expect(mockWrite.mock.calls).toEqual(lines.map(line => [`${line}\n`]));
+    }
+
+    function expectClearedLines(count) {
+        expect(mockMoveCursor.mock.calls)
+            .toEqual(Array.from({ length: count }).map(() => [process.stdout, 0, -1]));
+        expect(mockClearLine.mock.calls)
+            .toEqual(Array.from({ length: count }).map(() => [process.stdout, 0]));
+    }
+
+    describe('basics', () => {
+        it('toggle "keypress* subscription on and off', () => {
+            const promise = list([]);
+
+            expect(mockSetRawMode).toBeCalledWith(true);
+            expect(mockOn.mock.calls).toEqual([
+                ['keypress', expect.any(Function)],
+            ]);
+
+            emitReturn();
+
+            return promise.then(() => {
+                expect(mockSetRawMode).toBeCalledWith(false);
+                expect(mockOff.mock.calls).toEqual([
+                    ['keypress', mockOn.mock.calls[0][1]],
+                ]);
+            });
+        });
+
+        it('print and clear items', () => {
+            const promise = list(['a', 'b', 'c']);
+
+            expectPrintedLines(
+                '-[ ] a',
+                ' [ ] b',
+                ' [ ] c',
+            );
+
+            emitReturn();
+
+            return promise.then(() => {
+                expectClearedLines(3);
+            });
+        });
+
+        it('print multiline items', () => {
+            const promise = list(['Item 1\n a\n b', 'Item 2', 'Item 3\nnote']);
+
+            expectPrintedLines(
+                '-[ ] Item 1',
+                '      a',
+                '      b',
+                ' [ ] Item 2',
+                ' [ ] Item 3',
+                '     note',
+            );
+
+            emitReturn();
+
+            return promise.then(() => {
+                expectClearedLines(6);
+            });
+        });
+
+        it('reject on ESC', () => {
+            const promise = list([]);
+
+            emitKeypress({ sequence: '\u001b' });
+
+            return promise.then(
+                () => {
+                    throw new Error('should fail');
+                },
+                (err) => {
+                    expect(err.message).toEqual('Canceled');
+                }
+            );
+        });
+
+        it('reject on CTRL+C', () => {
+            const promise = list([]);
+
+            emitKeypress({ sequence: '\u0003' });
+
+            return promise.then(
+                () => {
+                    throw new Error('should fail');
+                },
+                (err) => {
+                    expect(err.message).toEqual('Canceled');
+                }
+            );
+        });
+    });
+
+    describe('options', () => {
+        it('set initial index', () => {
+            const promise = list(['a', 'b', 'c'], {
+                index: 1,
+            });
+
+            expectPrintedLines(
+                ' [ ] a',
+                '-[ ] b',
+                ' [ ] c',
+            );
+
+            emitReturn();
+
+            return promise.then(({ index }) => {
+                expect(index).toEqual(1);
+            });
+        });
+
+        it('set initial checks (multiple)', () => {
+            const promise = list(['a', 'b', 'c', 'd'], {
+                checks: [0, 2, 3],
+            });
+
+            expectPrintedLines(
+                '-[*] a',
+                ' [ ] b',
+                ' [*] c',
+                ' [*] d',
+            );
+
+            emitReturn();
+
+            return promise.then(({ checks }) => {
+                expect(checks).toEqual([0, 2, 3]);
+            });
+        });
+
+        it('set initial checks (single)', () => {
+            const promise = list(['a', 'b', 'c', 'd'], {
+                singleCheck: true,
+                checks: 2,
+            });
+
+            expectPrintedLines(
+                '-[ ] a',
+                ' [ ] b',
+                ' [*] c',
+                ' [ ] d',
+            );
+
+            emitReturn();
+
+            return promise.then(({ checks }) => {
+                expect(checks).toEqual(2);
+            });
+        });
+
+        it('print custom items', () => {
+            const printItem = jest.fn();
+            printItem.mockReturnValueOnce('<a1>\n<a2>');
+            printItem.mockReturnValueOnce();
+            printItem.mockReturnValueOnce('');
+            printItem.mockReturnValueOnce(0);
+
+            const promise = list(['a', 'b', 'c', 'd'], {
+                printItem,
+                checks: [1, 3],
+            });
+
+            expectPrintedLines(
+                '-[ ] <a1>',
+                '     <a2>',
+                ' [*] undefined',
+                ' [ ] ',
+                ' [*] 0',
+            );
+            expect(printItem.mock.calls).toEqual([
+                ['a', 0, false],
+                ['b', 1, true],
+                ['c', 2, false],
+                ['d', 3, true],
+            ]);
+
+            emitReturn();
+
+            return promise.then(() => {
+                expectClearedLines(5);
+            });
+        });
+    });
+
+    describe('keys handling', () => {
+        it('change index on "up"', () => {
+            const promise = list(['a', 'b', 'c'], {
+                index: 1,
+            });
+
+            mockWrite.mockClear();
+            emitKeypress({ name: 'up' });
+
+            expectClearedLines(3);
+            expectPrintedLines(
+                '-[ ] a',
+                ' [ ] b',
+                ' [ ] c',
+            );
+
+            mockMoveCursor.mockClear();
+            mockClearLine.mockClear();
+            emitReturn();
+
+            return promise.then(() => {
+                expectClearedLines(3);
+            });
+        });
+
+        it('change index on "down"', () => {
+            const promise = list(['a', 'b', 'c'], {
+                index: 1,
+            });
+
+            mockWrite.mockClear();
+            emitKeypress({ name: 'down' });
+
+            expectClearedLines(3);
+            expectPrintedLines(
+                ' [ ] a',
+                ' [ ] b',
+                '-[ ] c',
+            );
+
+            mockMoveCursor.mockClear();
+            mockClearLine.mockClear();
+            emitReturn();
+
+            return promise.then(() => {
+                expectClearedLines(3);
+            });
+        });
+
+        it('toggle check on "space" (on)', () => {
+            const promise = list(['a', 'b', 'c'], {
+                checks: [1],
+            });
+
+            mockWrite.mockClear();
+            emitKeypress({ name: 'space' });
+
+            expectClearedLines(3);
+            expectPrintedLines(
+                '-[*] a',
+                ' [*] b',
+                ' [ ] c',
+            );
+
+            mockMoveCursor.mockClear();
+            mockClearLine.mockClear();
+            emitReturn();
+
+            return promise.then(() => {
+                expectClearedLines(3);
+            });
+        });
+
+        it('toggle check on "space" (off)', () => {
+            const promise = list(['a', 'b', 'c'], {
+                checks: [0, 1],
+            });
+
+            mockWrite.mockClear();
+            emitKeypress({ name: 'space' });
+
+            expectClearedLines(3);
+            expectPrintedLines(
+                '-[ ] a',
+                ' [*] b',
+                ' [ ] c',
+            );
+
+            mockMoveCursor.mockClear();
+            mockClearLine.mockClear();
+            emitReturn();
+
+            return promise.then(() => {
+                expectClearedLines(3);
+            });
+        });
+
+        it('toggle check on "space" (on, single)', () => {
+            const promise = list(['a', 'b', 'c'], {
+                checks: 1,
+                singleCheck: true,
+            });
+
+            mockWrite.mockClear();
+            emitKeypress({ name: 'space' });
+
+            expectClearedLines(3);
+            expectPrintedLines(
+                '-[*] a',
+                ' [ ] b',
+                ' [ ] c',
+            );
+
+            mockMoveCursor.mockClear();
+            mockClearLine.mockClear();
+            emitReturn();
+
+            return promise.then(() => {
+                expectClearedLines(3);
+            });
+        });
+
+        it('toggle check on "space" (off, single)', () => {
+            const promise = list(['a', 'b', 'c'], {
+                index: 1,
+                checks: 1,
+                singleCheck: true,
+            });
+
+            mockWrite.mockClear();
+            emitKeypress({ name: 'space' });
+
+            expectClearedLines(3);
+            expectPrintedLines(
+                ' [ ] a',
+                '-[ ] b',
+                ' [ ] c',
+            );
+
+            mockMoveCursor.mockClear();
+            mockClearLine.mockClear();
+            emitReturn();
+
+            return promise.then(() => {
+                expectClearedLines(3);
+            });
+        });
+    });
+
+    describe('custom handlers', () => {
+        it('allow to change index', () => {
+            const mock = jest.fn(({ setIndex }) => {
+                setIndex(2);
+            });
+            const promise = list(['a', 'b', 'c'], {
+                handlers: {
+                    'h': mock,
+                },
+            });
+
+            mockWrite.mockClear();
+            emitKeypress({ name: 'h' });
+
+            expectClearedLines(3);
+            expectPrintedLines(
+                ' [ ] a',
+                ' [ ] b',
+                '-[ ] c',
+            );
+            expect(mock).toBeCalledWith({
+                index: 0, setIndex: expect.any(Function),
+                toggleCheck: expect.any(Function), end: expect.any(Function),
+            });
+
+            mockMoveCursor.mockClear();
+            mockClearLine.mockClear();
+            emitReturn();
+
+            return promise.then(() => {
+                expectClearedLines(3);
+            });
+        });
+
+        it('allow to toggle check', () => {
+            const mock = jest.fn(({ toggleCheck }) => {
+                toggleCheck(2);
+            });
+            const promise = list(['a', 'b', 'c'], {
+                handlers: {
+                    'j': mock,
+                },
+            });
+
+            mockWrite.mockClear();
+            emitKeypress({ name: 'j' });
+
+            expectClearedLines(3);
+            expectPrintedLines(
+                '-[ ] a',
+                ' [ ] b',
+                ' [*] c',
+            );
+            expect(mock).toBeCalledWith({
+                index: 0, setIndex: expect.any(Function),
+                toggleCheck: expect.any(Function), end: expect.any(Function),
+            });
+
+            mockMoveCursor.mockClear();
+            mockClearLine.mockClear();
+            emitReturn();
+
+            return promise.then(() => {
+                expectClearedLines(3);
+            });
+        });
+
+        it('allow to close', () => {
+            const mock = jest.fn(({ end }) => {
+                end('test');
+            });
+            const promise = list(['a', 'b', 'c'], {
+                handlers: {
+                    'k': mock,
+                },
+            });
+
+            mockWrite.mockClear();
+            emitKeypress({ name: 'k' });
+            expect(mock).toBeCalledWith({
+                index: 0, setIndex: expect.any(Function),
+                toggleCheck: expect.any(Function), end: expect.any(Function),
+            });
+
+            expectClearedLines(3);
+
+            return promise.then(({ note }) => {
+                expect(note).toEqual('test');
+            });
+        });
     });
 });
