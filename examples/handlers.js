3ee44548ed5f3f7c04aa10a024ea78947081ffb4
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2019-03-03 23:15:02 +0300

    Add more examples

---
+++ examples/handlers.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+
+const list = require('..');
+
+const items = ['A', 'B', 'C', 'D', 'E'];
+const options = {
+    handlers: {
+        'home': ({ setIndex }) => {
+            setIndex(0);
+        },
+        'end': ({ setIndex }) => {
+            setIndex(items.length - 1);
+        },
+        'j': ({ index, setIndex }) => {
+            setIndex(index - 1);
+        },
+        'k': ({ index, setIndex }) => {
+            setIndex(index + 1);
+        },
+        'l': ({ index, toggleCheck }) => {
+            toggleCheck(index);
+        },
+        'p': ({ end }) => {
+            end();
+        },
+    },
+};
+
+console.log('Go');
+list(items, options).then(
+    ({ checks }) => {
+        console.log(checks.map(i => items[i]).join(', '));
+    },
+    (err) => {
+        console.error(err);
+        process.exit(1);
+    }
+);
